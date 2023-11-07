becb8884449ebc5f1aeff8b5fca7b6f31114ed60
mojtabak-rob <mojtabakarbassi.2012@gmail.com>
2023-11-07 20:45:57 +0100

    make 2

--- Arduino/Exec_RLMIDI/Exec_RLMIDI.ino
+++ Arduino/Exec_RLMIDI/Exec_RLMIDI.ino
@@ -289,24 +289,6 @@ void setup() {
   move_init(ZROB2, ZROBPos2, false);
   delay(300);
 
-  //move_init(ZROB3, ZROBPos3, true);
-  //delay(100);
-
-  //move_init(ZROB4, ZROBPos4, true);
-  //delay(500);
-
-  //kick(ZROB1, 6, 9000);
-  //delay(300);
-
-  //kick(ZROB2, 6, 9000);
-  //delay(300);
-  
-
-  //kicks(ZROB3, 5, 2000);
-  //delay(100);
-
-  //kicks(ZROB4, 5, 2000);
-  //delay(100);
 
 
 }
@@ -315,92 +297,32 @@ void setup() {
 // Main Loop
 void loop() {
 
-  //read the length of ZRob1 kick
+  //read the number of notes
   while (!SERIAL.available()); 
-  unsigned long len1 = Serial.read(); 
+  unsigned long note_num = Serial.read(); 
 
-  // ZRob1 kick matrix decoding
-  unsigned long V1[len1][4]; 
-  for (int i=0; i<len1; i++){
-    for (int j=0; j<4; j++){
+  // Rhythm matrix decoding
+  unsigned long V1[note_num][3]; 
+  for (int i=0; i<note_num; i++){
+    for (int j=0; j<3; j++){
       while (!SERIAL.available());
-      V1[i][j] = Serial.read(); //V1[i][0]='start of note',
-                                //V1[i][1]='number of file',
-                                //V1[i][2]='Kick freq*10',
-                                //V1[i][3]='Kick amp',
+      V1[i][j] = Serial.read(); //V1[i][0]='ZRob number = [1,2]',
+                                //V1[i][1]='note duration (low byte) = [0 - 256] ms',
+                                //V1[i][2]='note duration (high byte) = [0 - 256] ms',                                
     }
   }
 
-  //read the length of ZRob2 kick
-  while (!SERIAL.available());
-  unsigned long len2 = Serial.read();
-
-  // ZRob2 kick matrix decoding
-  unsigned long V2[len2][4];
-  for (int i=0; i<len2; i++){
-    for (int j=0; j<4; j++){
-      while (!SERIAL.available());
-      V2[i][j] = Serial.read(); //V2[i][0]='start of note',
-                                //V2[i][1]='number of file',
-                                //V2[i][2]='Kick freq*10',
-                                //V2[i][3]='Kick amp',
-    }
+  // Latent space vector
+  unsigned long V2[4];
+  for (int i=0; i<4; i++){
+    while (!SERIAL.available());
+    V2[i] = Serial.read(); //V2[0]='freq1= [0 - 256]',
+                           //V2[1]='amp1= [0 - 256]',
+                           //V2[2]='freq2= [0 - 256]',
+                           //V2[3]='amp2= [0 - 256]',
+    
   }
 
-  //read the length of drum rolls
-  while (!SERIAL.available());
-  unsigned long len3 = Serial.read();
-
-  // Drum roll matrix decoding
-  unsigned long V3[len3][10];
-  for (int i=0; i<len3; i++){
-    for (int j=0; j<10; j++){
-      while (!SERIAL.available());
-      V3[i][j] = Serial.read(); //V3[i][0]='start of note',
-                                //V3[i][1]='end of note',
-                                //V3[i][2]='number of file'
-                                //V3[i][3]='amp1'
-                                //V3[i][4]='amp2'
-                                //V3[i][5]='idle1'
-                                //V3[i][6]='idle2'
-                                //V3[i][7]='freq1'
-                                //V3[i][8]='freq2'
-                                //V3[i][9]='shift'
-    }
-  }
-
-  //read the length of ZRob3 kick
-  while (!SERIAL.available());
-  unsigned long len4 = Serial.read();
-
-  // ZRob3 kick matrix decoding
-  unsigned long V4[len4][4];
-  for (int i=0; i<len4; i++){
-    for (int j=0; j<4; j++){
-      while (!SERIAL.available());
-      V4[i][j] = Serial.read(); //V4[i][0]='start of note',
-                                //V4[i][1]='number of file',
-                                //V4[i][2]='Kick freq*10',
-                                //V4[i][3]='Kick amp',
-    }
-  }
-
-  //read the length of ZRob4 kick
-  while (!SERIAL.available());
-  unsigned long len5 = Serial.read();
-
-  // ZRob4 kick matrix decoding
-  unsigned long V5[len5][4];
-  for (int i=0; i<len5; i++){
-    for (int j=0; j<4; j++){
-      while (!SERIAL.available());
-      V5[i][j] = Serial.read(); //V5[i][0]='start of note',
-                                //V5[i][1]='number of file',
-                                //V5[i][2]='Kick freq*10',
-                                //V5[i][3]='Kick amp',
-    }
-  }
-  
   // Validating SERIAL message
   while (!SERIAL.available());
   unsigned long CODE201 = Serial.read();
@@ -408,70 +330,29 @@ void loop() {
   // Execute the received message
   if (CODE201==201)
   {
-    unsigned long startmidi = millis(); //t0
-    bool done = false;
-    int n_right = 0;
-    int n_left = 0;
-    int n_roll = 0;
-    int n_3 = 0;
-    int n_4 = 0;
-    while (!done){
-      unsigned long tid = millis(); // t
-
-      //Play ZRob1 kick
-      for (int i=n_right; i<len1; i++){
-        tid = millis();
-        if (abs(tid-startmidi-(V1[i][0]+V1[i][1]*250)*100)<=5){
-          kick(ZROB1, 6+V1[i][2]*6/256, 9000+V1[i][3]*6000/256);
-          n_right++;
-        }
+    long ROB = 0;
+    float fr = 0;
+    long am = 0;
+    //unsigned long ntid = 0;
+    //unsigned long stid = millis();
+    for (int i=0; i<note_num; i++){
+      
+      if (V1[i][0]==1){
+        ROB = ZROB1;
+        fr = 6+V2[0]*4/256;
+        am = 6000+V2[1]*3000/256;
+      }
+      else {
+        ROB = ZROB2;
+        fr = 6+V2[2]*4/256;
+        am = 6000+V2[3]*3000/256;
       }
 
-      //Play ZRob2 kick
-      for (int i=n_left; i<len2; i++){
-        tid = millis();
-        if (abs(tid-startmidi-(V2[i][0]+V2[i][1]*250)*100)<=5){
-          kick(ZROB2, 6+V2[i][2]*6/256, 9000+V2[i][3]*6000/256);
-          n_left++;
-        }
-      }
-
-      //Play drum roll
-      for (int i=n_roll; i<len3; i++){
-        tid = millis();
-        if (abs(tid-startmidi-(V3[i][0]+V3[i][2]*250)*100)<=5){
-          roll(1000+V3[i][3]*20, 1000+V3[i][4]*20,
-              4000+V3[i][5]*20, 4000+V3[i][6]*20, 8+V3[i][7]*7/256,
-              8+V3[i][8]*7/256, V3[i][9],
-              (V3[i][1]-V3[i][0])*100);
-          n_roll++;
-        }
-      }
-
-      //Play ZRob3 kick
-      for (int i=n_3; i<len4; i++){
-        tid = millis();
-        if (abs(tid-startmidi-(V4[i][0]+V4[i][1]*250)*100)<=5){
-          kicks(ZROB3, 3+V4[i][2]*4/256, 1500+V4[i][3]*1500/256);
-          n_3++;
-        }
-      }
-
-      //Play ZRob4 kick
-      for (int i=n_4; i<len5; i++){
-        tid = millis();
-        if (abs(tid-startmidi-(V5[i][0]+V5[i][1]*250)*100)<=5){
-          kicks(ZROB4, 3+V5[i][2]*4/256, 1500+V4[i][3]*1500/256);
-          n_4++;
-        }
-      }
-
-      //Check if playing is done
-      //tid = millis();
-      if (n_left==len2 && n_right==len1 && n_roll==len3 &&
-          n_3==len4 && n_4==len5){
-        done = true;
-      }
+      kick(ROB, fr, am);
+      
+      delay(V1[i][1]+V1[i][2]*256-1000/fr-2);
     }
+
+      
   }
 }
--- Python/ZROB_ENVS.py
+++ Python/ZROB_ENVS.py
@@ -6,55 +6,7 @@ from gymnasium import spaces
 from numpy import linalg as LA
 
 class ZrobEnv(gym.Env):
-    def __init__(self, max_silence=2, min_silence=0.2, midi_seq=[1,0.3,1.7,0.5,0.2,0.3]):
-
-        self.num_notes = len(midi_seq)
-        self.max_silence = max_silence
-        self.min_silence = min_silence
-        self.midi_seq = midi_seq
-        
-
-        low = np.zeros((self.num_notes), dtype=np.float32) # type: ignore
-        high = np.ones((self.num_notes), dtype=np.float32)*self.max_silence
-
-        self.action_space = spaces.Box(low=0, high=self.max_silence, dtype=np.float32)
-        self.observation_space = spaces.Box(low=low, high=high, dtype=np.float32)
-
-    def step(self, u):
-        obs = self.state
-        done = True
-        cost = 0
-        midi_seq = self.midi_seq
-        
-        for i in range(len(obs)):
-            if obs[i]==0:
-                obs[i]=u
-                cost = (len(obs)-i)*abs(u-midi_seq[i])**2
-                if abs(u-midi_seq[i])<0.1:
-                    done=False
-                break
-
-        #for i in range(len(obs)):
-            #cost += (obs[i]-midi_seq[i])**2
-
-        self.state = obs
-        return self._get_obs(), -cost, done, False
-
-    def reset(self, *, seed: Optional[int] = None, options: Optional[dict] = None):
-        super().reset(seed=seed)
-
-        new = np.zeros((self.num_notes), dtype=np.float32)
-        self.state = new
-
-        return self._get_obs()
-
-    def _get_obs(self):
-
-        return self.state
-
-
-class ZrobEnv2(gym.Env):
-    def __init__(self,DoF=2):
+    def __init__(self,DoF=4):
 
         self.DoF=DoF
         
@@ -72,14 +24,7 @@ class ZrobEnv2(gym.Env):
         new = np.clip(new, lower_bound, upper_bound)
         self.state=new
         cost=0
-        dis = obs-self.target
-        cost1=(LA.norm(dis)**2-1)**2
-        #cost2=0
-        #if LA.norm(dis)>0.01:
-            #cost2=(u[0]/LA.norm(u)+dis[1]/LA.norm(dis))**2 + (u[1]/LA.norm(u)-dis[0]/LA.norm(dis))**2
-        #cost3=(LA.norm(u)**2-0.09*LA.norm(dis)**2)**2
-        #cost=cost1+5*cost2+cost3
-        cost=cost1
+        
         done=False
         
         return self._get_obs(), -cost, done, False
